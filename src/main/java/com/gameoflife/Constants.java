8c82edc4500938eec59378ed57fb1cf9b66743c1
Vlad Valean <vlad.valean19@gmail.com>
2025-11-11 10:01:53 +0200

    added RaportProiect.pdf

--- src/main/java/com/gameoflife/Constants.java
+++ src/main/java/com/gameoflife/Constants.java
@@ -2,12 +2,12 @@ package com.gameoflife;
 
 public final class Constants {
 
-    public static final int WORLD_WIDTH = 20;
-    public static final int WORLD_HEIGHT = 10;
+    public static final int WORLD_WIDTH = 100;
+    public static final int WORLD_HEIGHT = 50;
 
-    public static final long VISUALIZER_REFRESH_MS = 50;
+    public static final long VISUALIZER_REFRESH_MS = 100;
 
-    public static final int INITIAL_FOOD_UNITS = 10;
+    public static final int INITIAL_FOOD_UNITS = 100;
     public static final int FOOD_DROPPED_ON_DEATH_MIN = 1;
     public static final int FOOD_DROPPED_ON_DEATH_MAX = 5;
 
@@ -17,8 +17,8 @@ public final class Constants {
     public static final long SIM_STEP_MS = 100;
     public static final int MIN_MEALS_TO_REPRODUCE = 3;
 
-    public static final int INITIAL_ASEXUAL_CELLS = 20;
-    public static final int INITIAL_SEXUAL_CELLS = 20;
+    public static final int INITIAL_ASEXUAL_CELLS = 15;
+    public static final int INITIAL_SEXUAL_CELLS = 15;
 
     public static final long SHUTDOWN_DELAY_MS = 5000;
     public static final long MONITOR_THREAD_SLEEP_MS = 2000;
